253f50f6531a903ad8202f2d88e85260d91266cd
rohitjha9051 <rohitjha9051@gmail.com>
2025-12-06 19:35:51 +0530

    new updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,43 @@
-import React from 'react'
-import GrievanceForm from './Form/Form'
+import React, { useEffect, useState, useRef } from "react";
+import GrievanceForm from "./Form/Form";
+import "./Overlay.css";
 
 function App() {
+  const [showOverlay, setShowOverlay] = useState(false);
+  const timerRef = useRef(null);
+
+  // Start 5-second inactivity timer
+  useEffect(() => {
+    timerRef.current = setTimeout(() => {
+      setShowOverlay(true);
+    }, 5000);
+
+    return () => clearTimeout(timerRef.current);
+  }, []);
+
+  // Remove overlay on click/touch
+  const handleUserActivity = () => {
+    clearTimeout(timerRef.current);
+    setShowOverlay(false);
+  };
+
   return (
     <>
-    <GrievanceForm/>
+      {/* AUTO OVERLAY */}
+      {showOverlay && (
+        <div className="overlay" onClick={() => setShowOverlay(false)}>
+          <div className="overlay-slider">
+            <img src="/img1.jpg.jpg" alt="Overlay" />
+          </div>
+        </div>
+      )}
+
+      {/* ORIGINAL FORM */}
+      <div onClick={handleUserActivity} onTouchStart={handleUserActivity}>
+        <GrievanceForm />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
